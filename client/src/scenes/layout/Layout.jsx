2930acc4a04af07023274f7d2162cd1112a5c7c8
dsubotic <dragan.subi7@gmail.com>
2023-03-03 21:22:40 +0100

    add sidebar layout

--- client/src/scenes/layout/Layout.jsx
+++ client/src/scenes/layout/Layout.jsx
@@ -2,13 +2,28 @@ import { useState } from "react";
 import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { Navbar } from "../components"
+import { Navbar, Sidebar } from "../components"
 
 export default function Layout() {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   return (
-    <Box width="100%" height="100%">
+    <Box
+      display={isNonMobile ? "flex" : "block"}
+      width="100%"
+      height="100%"
+    >
+      <Sidebar
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
       <Box>
-        <Navbar />
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
         <Outlet />
       </Box>
     </Box>
